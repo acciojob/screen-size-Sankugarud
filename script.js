05c97c8a99c964b7376913b1653051844011971f
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-05-31 02:18:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ let headingtag = document.createElement("h1");
 let div = document.getElementById("sizeInfo");
 window.onresize = function(){
 	headingtag.innerHTML = `Width: ${innerWidth} and Height: ${innerHeight}`
+	div.appendChild(headingtag);
 }
-div.appendChild(headingtag)
+
