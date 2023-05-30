7a333ad05c68759b50ff02c1685a42230e912805
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-05-31 01:24:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let headingtag = document.createElement("h1");
+let div = document.getElementById("sizeInfo");
+window.onresize = function(){
+	headingtag.innerHTML = `width: ${window.innerWidth} and height: ${window.innerHeight}`
+}
+div.append(headingtag);
